3d1953b4cc51284762a705de7e652905150ba79f
Rommel Rico <rommeltj@gmail.com>
2016-08-21 13:04:28 -0700

    Create AllChatsViewController

---
+++ WhaleTalk/AllChatsViewController.swift
@@ -0,0 +1,25 @@
+//
+//  AllChatsViewController.swift
+//  WhaleTalk
+//
+//  Created by Rommel Rico on 8/21/16.
+//  Copyright © 2016 Rommel Rico. All rights reserved.
+//
+
+import UIKit
+import CoreData
+
+class AllChatsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
