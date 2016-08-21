4e0762d9b7b080210f9b94cd53191ef5cfb2bec7
Rommel Rico <rommeltj@gmail.com>
2016-08-21 12:49:49 -0700

    Create a lastMessage computed attribute

--- WhaleTalk/Chat.swift
+++ WhaleTalk/Chat.swift
@@ -12,6 +12,19 @@ import CoreData
 
 class Chat: NSManagedObject {
 
-// Insert code here to add functionality to your managed object subclass
+    // Insert code here to add functionality to your managed object subclass
+    var lastMessage: Message? {
+        let request = NSFetchRequest(entityName: "Message")
+        request.predicate = NSPredicate(format: "chat = %@", self)
+        request.sortDescriptors = [NSSortDescriptor(key: "timestamp", ascending: false)]
+        request.fetchLimit = 1
+        do {
+            guard let results = try self.managedObjectContext?.executeFetchRequest(request) as? [Message] else { return nil }
+            return results.first
+        } catch {
+            print("Error for request")
+        }
+        return nil
+    }
 
 }
