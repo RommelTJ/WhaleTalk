8b9473b86adb89f8a3f030fb09e57b455d620d48
Rommel Rico <rommeltj@gmail.com>
2016-08-21 09:11:25 -0700

    Create a Message NSManagedObject

---
+++ Message.swift
@@ -0,0 +1,17 @@
+//
+//  Message.swift
+//  WhaleTalk
+//
+//  Created by Rommel Rico on 8/21/16.
+//  Copyright © 2016 Rommel Rico. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+
+class Message: NSManagedObject {
+
+// Insert code here to add functionality to your managed object subclass
+
+}
