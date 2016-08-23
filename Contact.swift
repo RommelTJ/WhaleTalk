a8ed94e8d78921d15e22b90e03f967e39c50440b
Rommel Rico <rommeltj@gmail.com>
2016-08-22 18:24:48 -0700

    Update the Contact Entity

---
+++ Contact.swift
@@ -0,0 +1,17 @@
+//
+//  Contact.swift
+//  WhaleTalk
+//
+//  Created by Rommel Rico on 8/22/16.
+//  Copyright © 2016 Rommel Rico. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+
+class Contact: NSManagedObject {
+
+// Insert code here to add functionality to your managed object subclass
+
+}
