da9e20cfb855bbf315684326f16ea7ef9d86590f
Rommel Rico <rommeltj@gmail.com>
2016-08-25 19:58:27 -0700

    Implement createSearchField method

--- WhaleTalk/NewGroupParticipantsViewController.swift
+++ WhaleTalk/NewGroupParticipantsViewController.swift
@@ -14,6 +14,7 @@ class NewGroupParticipantsViewController: UIViewController {
     var context: NSManagedObjectContext?
     var chat: Chat?
     var chatCreationDelegate: ChatCreationDelegate?
+    private var searchField: UITextField!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -24,6 +25,25 @@ class NewGroupParticipantsViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    private func createSearchField() -> UITextField {
+        let searchField = UITextField(frame: CGRect(x: 0, y: 0, width: 0, height: 50))
+        searchField.backgroundColor = UIColor(red: 220/255, green: 220/255, blue: 220/255, alpha: 1.0)
+        searchField.placeholder = "Type contact name"
+        
+        let holderView = UIView(frame: CGRect(x: 0, y: 0, width: 50, height: 50))
+        searchField.leftView = holderView
+        searchField.leftViewMode = .Always
+        
+        let image = UIImage(named: "contact_icon")?.imageWithRenderingMode(.AlwaysTemplate)
+        let contactImage = UIImageView(image: image)
+        contactImage.tintColor = UIColor.darkGrayColor()
+        
+        holderView.addSubview(contactImage)
+        contactImage.translatesAutoresizingMaskIntoConstraints = false
+        
+        return searchField
     }
 
 }
