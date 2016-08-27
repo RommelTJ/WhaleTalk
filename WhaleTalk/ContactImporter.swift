d62cfbfdfdeed6b4fa70775bac55dcec5a7b4c42
Rommel Rico <rommeltj@gmail.com>
2016-08-27 07:54:59 -0700

    Make Fetch thread safe and save

--- WhaleTalk/ContactImporter.swift
+++ WhaleTalk/ContactImporter.swift
@@ -29,34 +29,38 @@ class ContactImporter {
     func fetch() {
         let store = CNContactStore()
         store.requestAccessForEntityType(.Contacts) { (granted, error) in
-            if granted {
-                do {
-                    let req = CNContactFetchRequest(keysToFetch: [
-                        CNContactGivenNameKey,
-                        CNContactFamilyNameKey,
-                        CNContactPhoneNumbersKey
-                        ])
-                    try store.enumerateContactsWithFetchRequest(req, usingBlock: { (cnContact, stop) in
-                        guard let contact = NSEntityDescription.insertNewObjectForEntityForName("Contact", inManagedObjectContext: self.context) as? Contact else { return }
-                        
-                        contact.firstName = cnContact.givenName
-                        contact.lastName = cnContact.familyName
-                        contact.contactId = cnContact.identifier
-                        let contactNumbers = NSMutableSet()
-                        for cnVal in cnContact.phoneNumbers {
-                            guard let cnPhoneNumber = cnVal.value as? CNPhoneNumber else { continue }
-                            guard let phoneNumber = NSEntityDescription.insertNewObjectForEntityForName("PhoneNumber", inManagedObjectContext: self.context) as? PhoneNumber else { continue }
-                            phoneNumber.value = self.formatPhoneNumber(cnPhoneNumber)
-                            contactNumbers.addObject(phoneNumber)
-                        }
-                        contact.phoneNumbers  = contactNumbers
-                    })
-                } catch let error as NSError {
-                    print(error)
-                } catch {
-                    print("Error with do-catch")
+            
+            self.context.performBlock({ 
+                if granted {
+                    do {
+                        let req = CNContactFetchRequest(keysToFetch: [
+                            CNContactGivenNameKey,
+                            CNContactFamilyNameKey,
+                            CNContactPhoneNumbersKey
+                            ])
+                        try store.enumerateContactsWithFetchRequest(req, usingBlock: { (cnContact, stop) in
+                            guard let contact = NSEntityDescription.insertNewObjectForEntityForName("Contact", inManagedObjectContext: self.context) as? Contact else { return }
+                            
+                            contact.firstName = cnContact.givenName
+                            contact.lastName = cnContact.familyName
+                            contact.contactId = cnContact.identifier
+
+                            for cnVal in cnContact.phoneNumbers {
+                                guard let cnPhoneNumber = cnVal.value as? CNPhoneNumber else { continue }
+                                guard let phoneNumber = NSEntityDescription.insertNewObjectForEntityForName("PhoneNumber", inManagedObjectContext: self.context) as? PhoneNumber else { continue }
+                                phoneNumber.value = self.formatPhoneNumber(cnPhoneNumber)
+                                phoneNumber.contact = contact
+                            }
+                        })
+                        try self.context.save()
+                    } catch let error as NSError {
+                        print(error)
+                    } catch {
+                        print("Error with do-catch")
+                    }
                 }
-            }
+            })
+            
         }
     }
     
