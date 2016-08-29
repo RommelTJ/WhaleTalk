5298154ccb30f7d60a530b55780df21c950108a5
Rommel Rico <rommeltj@gmail.com>
2016-08-28 20:35:34 -0700

    Create ContactSelector

---
+++ WhaleTalk/ContactSelector.swift
@@ -0,0 +1,13 @@
+//
+//  ContactSelector.swift
+//  WhaleTalk
+//
+//  Created by Rommel Rico on 8/28/16.
+//  Copyright © 2016 Rommel Rico. All rights reserved.
+//
+
+import Foundation
+
+protocol ContactSelector {
+    func selectedContact(contact: Contact)
+}
