4b8468484ea3db8cf34445ebc9fd77aa7e76dfff
Rommel Rico <rommeltj@gmail.com>
2016-08-28 21:12:19 -0700

    Create FavoritesViewController

---
+++ WhaleTalk/FavoritesViewController.swift
@@ -0,0 +1,34 @@
+//
+//  FavoritesViewController.swift
+//  WhaleTalk
+//
+//  Created by Rommel Rico on 8/28/16.
+//  Copyright © 2016 Rommel Rico. All rights reserved.
+//
+
+import UIKit
+import CoreData
+import Contacts
+import ContactsUI
+
+class FavoritesViewController: UIViewController, TableViewFetchedResultsDisplayer, ContextViewController {
+    
+    var context: NSManagedObjectContext?
+    private var fetchedResultsController: NSFetchedResultsController?
+    private var fetchedResultsDelegate: NSFetchedResultsControllerDelegate?
+    private let tableView = UITableView(frame: CGRectZero, style: .Plain)
+    private let cellIdentifier = "FavoriteCell"
+    private let store = CNContactStore()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
