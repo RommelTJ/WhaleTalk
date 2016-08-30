d7c846bac2b5713c33fd2b6b0d0f89ab52363531
Rommel Rico <rommeltj@gmail.com>
2016-08-29 21:30:10 -0700

    Continue setup

--- WhaleTalk/SignUpViewController.swift
+++ WhaleTalk/SignUpViewController.swift
@@ -29,6 +29,28 @@ class SignUpViewController: UIViewController {
         continueButton.setTitleColor(UIColor.blackColor(), forState: .Normal)
         continueButton.addTarget(self, action: #selector(SignUpViewController.pressedContinue(_:)), forControlEvents: .TouchUpInside)
         view.addSubview(continueButton)
+        
+        phoneNumberField.keyboardType = .PhonePad
+        label.translatesAutoresizingMaskIntoConstraints = false
+        continueButton.translatesAutoresizingMaskIntoConstraints = false
+        
+        let fields = [
+            (phoneNumberField, "Phone Number"),
+            (emailField, "Email"),
+            (passwordField, "Password")
+        ]
+        fields.forEach {
+            $0.0.placeholder = $0.1
+        }
+        
+        passwordField.secureTextEntry = true
+        
+        let stackView = UIStackView(arrangedSubviews: fields.map{$0.0})
+        stackView.axis = .Vertical
+        stackView.alignment = .Fill
+        stackView.spacing = 20
+        view.addSubview(stackView)
+        stackView.translatesAutoresizingMaskIntoConstraints = false
     }
 
     override func didReceiveMemoryWarning() {
