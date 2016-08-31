cf893552451511e61b1c69e2b82ecc830230be33
Rommel Rico <rommeltj@gmail.com>
2016-08-30 19:59:57 -0700

    Query Firebase Contact

--- WhaleTalk/FirebaseModel.swift
+++ WhaleTalk/FirebaseModel.swift
@@ -8,8 +8,24 @@
 
 import Foundation
 import Firebase
+import FIRDatabase
 import CoreData
 
 protocol FirebaseModel {
     func upload(context: NSManagedObjectContext)
+}
+
+extension Contact: FirebaseModel {
+    
+    func upload(context: NSManagedObjectContext) {
+        guard let phoneNumbers = phoneNumbers?.allObjects as? [PhoneNumber] else { return }
+        for number in phoneNumbers {
+            let reference = FIRDatabase.database().reference()
+            reference.child("users").queryOrderedByChild("phoneNumber").queryEqualToValue(number.value).observeSingleEventOfType(.Value, withBlock: {
+                snapshot in
+                //TODO
+            })
+        }
+    }
+    
 }
